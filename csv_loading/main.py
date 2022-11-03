5fe1f2c08c8fc2073e7b20ece73ee76b0df2a62e
Antony García <agarcia3@wpi.edu>
2022-11-03 10:33:45 -0400

    Initial commit

---
+++ csv_loading/main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def loadDataset(fileName, samples):
+    x = []
+    y = []
+    train_data = pd.read_csv(fileName)
+    y = train_data.iloc[0:samples, 0]
+    x = train_data.iloc[0:samples, 1:]
+    return x,y
+
+x,y=loadDataset("train.csv",100)
+print(x.shape)
+print(y.shape)
