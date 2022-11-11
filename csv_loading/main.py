40ff0043c2a12b19a808fd75b6745da3917bc0f1
Antony García <agarcia3@wpi.edu>
2022-11-11 17:19:16 -0500

    Agregado el ejemplo de conversión de MNIST a imágenes

--- csv_loading/main.py
+++ csv_loading/main.py
@@ -8,6 +8,6 @@ def loadDataset(fileName, samples):
     x = train_data.iloc[0:samples, 1:]
     return x,y
 
-x,y=loadDataset("train.csv",100)
+x,y=loadDataset("../datasets/mnist/mnist_train.csv",100)
 print(x.shape)
 print(y.shape)
