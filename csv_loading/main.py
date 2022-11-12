2b37f012187e55088519e01dde4ca6c87fc90500
Antony García <agarcia3@wpi.edu>
2022-11-12 17:51:07 -0500

    More updates for the second post

--- csv_loading/main.py
+++ csv_loading/main.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import numpy as np
 
 def loadDataset(fileName, samples):
     x = []
     y = []
     train_data = pd.read_csv(fileName)
-    y = train_data.iloc[0:samples, 0]
-    x = train_data.iloc[0:samples, 1:]
+    y = np.array(train_data.iloc[0:samples, 0])
+    x = np.array(train_data.iloc[0:samples, 1:])
     return x,y
 
 x,y=loadDataset("../datasets/mnist/mnist_train.csv",100)
--- mnist_to_image/main.py
+++ mnist_to_image/main.py
@@ -1,13 +1,18 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 def loadDataset(fileName, samples):
     x = []
     y = []
     train_data = pd.read_csv(fileName)
-    y = train_data.iloc[0:samples, 0]
-    x = train_data.iloc[0:samples, 1:]
+    y = np.array(train_data.iloc[0:samples, 0])
+    x = np.array(train_data.iloc[0:samples, 1:])
     return x,y
 
 x,y=loadDataset("../datasets/mnist/mnist_train.csv",100)
-print(x.shape)
-print(y.shape)
+
+digit = x[0]
+digit_pixels = digit.reshape(28, 28)
+plt.imshow(digit_pixels, cmap='gray')
+plt.show()
