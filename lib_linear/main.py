8e72a789b615a87a4a5c79ff00258f109136a58a
Antony García <agarcia3@wpi.edu>
2022-11-15 19:24:17 -0500

    liblinear

--- lib_linear/main.py
+++ lib_linear/main.py
@@ -1,21 +1,82 @@
-import pandas as pd
-import numpy as np
-from liblinear.liblinear import problem, parameter, liblinear
+import csv
+from sklearn import svm
+import time
+
+from sklearn.linear_model import LogisticRegression
 
 
-def loadDataset(fileName, samples):
-    x = []
-    y = []
-    train_data = pd.read_csv(fileName)
-    y = np.array(train_data.iloc[0:samples, 0])
-    x = np.array(train_data.iloc[0:samples, 1:])/255
-    return x,y
+def main():
 
-train_x,train_y=loadDataset("../datasets/mnist/mnist_train.csv",1000)
-prob = problem(train_y, train_x)
-param = parameter('-c 4')
-m = liblinear.train(prob, param)
+    resultColumn  =0
+    minDataColumn = 1
+    maxDataColumn = 785
+    x=[]
+    y=[]
+    resultCount=0
+    validResults=0
+    trainingSize = 50000
+    testingSize = 10000
+    trainingCounter =0
+    testingCounter=0
+    startTime=0
+    endTime=0
 
-test_x,test_y=loadDataset("../datasets/mnist/mnist_test.csv",100)
-p_labels, p_acc, p_vals = liblinear.predict(test_y, test_x, m)
-print(p_acc)
+    clf = None
+    print("Training...")
+    with open('../datasets/mnist/mnist_train.csv') as csvfile :
+        reader = csv.reader(csvfile)
+        next(reader)
+        for row in reader:
+            temp = []
+            for i in range(minDataColumn,maxDataColumn):
+                num = int(row[i])/255
+                temp.append(num)
+            x.append(temp)
+            y.append(row[resultColumn])
+            if (trainingCounter >= trainingSize):
+                break
+            else:
+                trainingCounter = trainingCounter + 1
+        clf = LogisticRegression(solver='liblinear')
+        startTime=time.time()
+        clf.fit(x, y)
+        endTime = time.time()
+    data = []
+    print("Testing...")
+    with open('../datasets/mnist/mnist_test.csv') as testfile:
+        reader = csv.reader(testfile)
+        next(reader)
+        for row in reader:
+            temp = []
+            res = []
+            for i in range(minDataColumn, maxDataColumn):
+                num = int(row[i]) / 255
+                temp.append(num)
+            expectedResult = row[resultColumn]
+            result = clf.predict([temp])
+            res.append(row[resultColumn])
+            res.append(result)
+            data.append(res)
+            resultCount=resultCount+1
+            outcome = "Fail"
+            if expectedResult==result:
+                validResults=validResults+1
+                outcome = "OK"
+            print("Test N: ",resultCount," | Expected result: ",expectedResult," | Obtained result: ",result, " | Outcome: ",outcome ," | Accuracy: ",(validResults/resultCount)*100,"%");
+            if (testingCounter >= testingSize-1):
+                break
+            else:
+                testingCounter = testingCounter + 1
+
+    print("--------------------")
+    print("Results")
+    print("--------------------")
+    print("Performed tests: ",resultCount)
+    print("Valid results: ", validResults)
+    print("Accuracy: ",(validResults/resultCount)*100,"%")
+    print("Training time: ", endTime-startTime)
+
+if __name__ == "__main__":
+    main()
+
+
--- lib_svm/main.py
+++ lib_svm/main.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
-import sys
+import time
+
 from libsvm.svmutil import svm_train, svm_predict
 
-
+startTime=0
+endTime=0
 def loadDataset(fileName, samples):
     x = []
     y = []
@@ -13,8 +15,15 @@ def loadDataset(fileName, samples):
     return x,y
 
 train_x,train_y=loadDataset("../datasets/mnist/mnist_train.csv",50000)
+startTime=time.time()
 m = svm_train(train_y, train_x, '-s 0 -d 1 -g 0.025 -c 100 -t 2 -q')
+endTime=time.time()
 
 test_x,test_y=loadDataset("../datasets/mnist/mnist_test.csv",10000)
 p_label, p_acc, p_val = svm_predict(test_y, test_x, m,options="-q")
-print(p_acc)
+
+print("--------------------")
+print("Results")
+print("--------------------")
+print("Accuracy: ", p_acc, "%")
+print("Training time: ", endTime - startTime," s")
